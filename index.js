c105e94d8649030dd33647685640c3f6766f4620
Beefox <bee@beefox.xyz>
2023-06-08 06:17:03 +1000

    add more things

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const text = [
 ];
 
 const hats = {
+  //Squid
   'chef': {
     src: 'squid-hat.png',
     alt: "A giant squid, adorned with a chef's hat, floating ominously on the page",
@@ -84,24 +85,53 @@ const hats = {
     title: "Giant Squid by Delapouite at game-icons.net",
 	contentType: 'squid'
   },
-  'ballclark': {
-    src: 'ballclark.png',
-    alt: 'An angry bbaseball, floating ominously on the page',
-    title: "Ball Clark by Quinns at People Make Games",
-	contentType: 'squid'
-  },
   'sunglasses': {
     src: 'sunglasses-squid.png',
     alt: 'A giant squid, adorned with sunglasses, floating ominously on the page',
     title: "The Monitor with sunglasses from Blaseball.com",
 	contentType: 'squid'
   },
+  '3d': {
+    src: '1024px-Tgb_monitor.png',
+    alt: 'A giant squid, with a foam finger, popcorn and glowing red eyes, floating ominously on the page',
+    title: "The 3d Monitor from Blaseball.com",
+	contentType: 'squid'
+  },
+  '2d': {
+    src: '2d.png',
+    alt: 'A giant squid, with a foam finger, popcorn and glowing red eyes drawn poorly, floating ominously on the page',
+    title: "The badly drawn 3d Monitor from Blaseball.com",
+	contentType: 'squid'
+  },
+  'wave': {
+    src: 'blinky-wave.gif',
+    alt: 'A giant squid, with a foam finger and empty popcorn, drawn waving poorly, floating ominously on the page',
+    title: "The badly drawn Monitor from Blaseball.com",
+	contentType: 'squid'
+  },
+
+  //Coin
   'coin': {
+    src: "Equity.c533a2ae.png",
+    alt: "A roman equity coin",
+    title: "The Coin from Blaseball.com",
 	contentType: 'coin'
   },
   'scattered': {
+    src: "scattered.png",
+    alt: "A glitchy roman equity coin",
+    title: "The Coin, scattered, from Blaseball.com",
 	contentType: 'scattered'  
   },
+  'melting': {
+    src: "Bossmelt.png",
+    alt: "A melting roman equity coin",
+    title: "The Coin, melting, from Blaseball.com",
+	contentType: 'melting'  
+  },
+  
+
+  //Peanut
   'shelled-one': {
     src: 'peanut.svg',
     alt: 'A giant peanut, spinning ominously on the page',
@@ -120,6 +150,50 @@ const hats = {
     title: 'Peanut from Blaseball.com',
 	contentType: 'tutorial'
   },
+
+  //Microphone
+  'microphone':{
+    src:"feedback.png",
+    alt:"Pink Microphone",
+    title: "Microphone from Blaseball.com",
+    contentType: "microphone"
+  },
+  'microphone-tweet':{
+    src:"MicrophoneTwitter.jpg",
+    alt:"Pink Microphone",
+    title: "Microphone from Blaseball.com",
+    contentType: "microphone-tweet"
+  },
+
+  //Reader
+  'reader':{
+    src:"Tgb_desert.png",
+    alt:"An ominous purple eye",
+    title: "The Reader from Blaseball.com",
+    contentType: "reader"
+  },
+
+  //Lōotcrates
+  'lootcrates':{
+    src:"Lootcrates.png",
+    alt:"A stack of crates",
+    title: "Lōotcrates from Blaseball.com",
+    contentType: "lootcrates"
+  },
+
+  //Namerifeht
+  'namerifeht':{
+    contentType: "namerifeht"
+  },
+
+  //Misc
+  'ballclark': {
+    src: 'ballclark.png',
+    alt: 'An angry bbaseball, floating ominously on the page',
+    title: "Ball Clark by Quinns at People Make Games",
+	contentType: 'squid'
+  },
+
 };
 
 const textArray = text.map(saying => Array(saying.weight).fill(saying.text)).flat();
@@ -139,10 +213,16 @@ function onLoad() {
 	if (type === 'shelled-two' || type === 'tutorial'){
 		document.querySelector(".Overlay-Text-Line").className = "Overlay-Text-Line text"
     }
+    if (type === 'namerifeht') {
+        document.querySelector(".icon").innerHTML = "<video autoplay loop src=n.webm>"
+    }
 
     const text = params.get('text');
-    if (text) {
+    if (text && type !== 'namerifeht') {
         document.querySelector(".text").textContent = text;
+    }
+    else if (text && type === 'namerifeht'){
+        document.querySelector(".text").innerHTML = text.split("").map(string=>`<span>${string}</span>`).join("");
     } else {
         setText();
     }
